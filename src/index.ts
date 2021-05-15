ea436b043b6c344c566331d3e379f4c741b53b67
Vicente Santiago <vicentejsp@outlook.com>
2021-05-14 22:26:17 -0500

    Clase 14: Observer y Subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<any> = {
+	next: value => console.log(`Next [Observer]: ${value}`),
+	error: error => console.warn(`Error [Observer]: ${error}`),
+	complete: () => console.info('Completado [Observer]'),
+}
 
 // const obs$ = Observable.crate();
-const obs$ = new Observable<string>( subs => {
+const obs$ = new Observable<string>(subs => {
 	subs.next('Hola');
 	subs.next('Jennifer');
 
 	subs.next('Hola');
 	subs.next('Jennifer');
+
+	// Forzar error
+	// let a = undefined;
+	// a.nombre = 'Vicente';
 
 	subs.complete();
 
@@ -15,4 +25,10 @@ const obs$ = new Observable<string>( subs => {
 
 });
 
-obs$.subscribe( console.log );
+// obs$.subscribe(
+// 	valor => console.log(`Next: ${valor}`),
+// 	err => console.warn(`Error: ${err}`),
+// 	() => console.info('Completado')
+// );
+
+obs$.subscribe(observer);
