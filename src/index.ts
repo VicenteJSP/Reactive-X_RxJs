fd1736abd2f79dcd696c4fc0c6863eb81856093c
Vicente Santiago <vicentejsp@outlook.com>
2021-05-14 23:31:26 -0500

    Clase 16: Subscription y unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import { Observable, Observer } from 'rxjs';
 
 const observer: Observer<any> = {
-	next: value => console.log(`Next [Observer]: ${value}`),
-	error: error => console.warn(`Error [Observer]: ${error}`),
-	complete: () => console.info('Completado [Observer]'),
+	next: value => console.log(`next: ${value}`),
+	error: error => console.warn(`error: ${error}`),
+	complete: () => console.info('completado!'),
 }
+
+const intervalo$ = new Observable<number>(
+	subscriber => {
+		let numero = 0;
+		const interval = setInterval( () => {
+			numero++;
+			subscriber.next(numero);
+			console.log(numero);
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+			console.log('Intervalo destruido!');
+		}
+	}
+);
+
+const subscription = intervalo$.subscribe(numero => console.log(`numero: ${numero}`));
+
+setTimeout(() => {
+	subscription.unsubscribe();
+}, 3000);
