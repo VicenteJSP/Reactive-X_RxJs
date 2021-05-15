2903dc90f990de62681c06a51950805ad606a91a
Vicente Santiago <vicentejsp@outlook.com>
2021-05-14 19:00:35 -0500

    Clase 13: Nuestro primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import { Observable } from 'rxjs';
 
+// const obs$ = Observable.crate();
+const obs$ = new Observable<string>( subs => {
+	subs.next('Hola');
+	subs.next('Jennifer');
 
-console.log('Hola Mundo!');
+	subs.next('Hola');
+	subs.next('Jennifer');
 
+	subs.complete();
 
+	subs.next('Hola');
+	subs.next('Jennifer');
 
+});
 
-
-
-
+obs$.subscribe( console.log );
