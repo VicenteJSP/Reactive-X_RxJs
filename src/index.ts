3525da1d65a5bc90f6a886df22079429bc27795f
Vicente Santiago <vicentejsp@outlook.com>
2021-05-17 19:08:58 -0500

    Clase 37: operador filter

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { range, from } from 'rxjs';
+import { filter, pluck } from 'rxjs/operators';
+
+const obs$ = range(20, 30);
+// const obsFilter$ = obs$.pipe(filter( e => e % 2 === 1));
+const obsFilter$ = obs$.pipe(filter((e, i) => {
+    console.log('index', i);
+    return e % 2 === 1
+}));
+
+interface Personaje {
+    nombre: string;
+    tipo: string;
+}
+
+const personajes: Array<Personaje> = [
+    {
+        tipo: 'heroe',
+        nombre: 'Batman'
+    },
+    {
+        tipo: 'heroe',
+        nombre: 'Robin'
+    },
+    {
+        tipo: 'villano',
+        nombre: 'Joker'
+    },
+]
+
+const obsPersonajes$ = from(personajes);
+const heroe$ = obsPersonajes$
+    .pipe(filter(p => p.tipo === 'heroe'));
+const villano$ = obsPersonajes$
+    .pipe(filter(p => p.tipo === 'villano'))
+
+// obsFilter$.subscribe(console.log);
+// heroe$.subscribe(console.log);
+villano$.subscribe(console.log);
