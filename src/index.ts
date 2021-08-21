d0bf4c0fdcba1250b875cd0322b639a1a41b6d28
Vicente Santiago <vicentejsp@outlook.com>
2021-08-21 09:09:38 -0500

    Clase 67: Manejo de errores con Fetch API

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ const url = 'https://api.github.com/useXrs?per_page=5';
 
 const fetchPromesa = fetch(url);
 
+const manejaErrores = (res: Response) => {
+    if (!res.ok) {
+        throw new Error(res.statusText);
+    }
+    return res;
+};
+
 fetchPromesa
-    .then(res => res.json())
+    .then(manejaErrores)
+    .then(res => (res).json())
     .then(data => console.log('Data:', data))
-    .catch(err => console.log('Error en usuarios', err))
+    .catch(err => console.warn('Error en usuarios', err))
