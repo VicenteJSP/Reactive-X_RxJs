85c6c2da8e3a7e25b2d18f2aafb081b56dd139a0
Vicente Santiago <vicentejsp@outlook.com>
2021-09-16 22:21:43 -0500

    Clase 93: CombineLatest Deprecated

---
+++ src/combinacion/05-combineLatest.ts
@@ -0,0 +1,31 @@
+import { combineLatest, fromEvent } from "rxjs";
+import { pluck } from "rxjs/operators";
+
+// const keyup$ = fromEvent(document, 'keyup');
+// const click$ = fromEvent(document, 'click');
+
+// combineLatest(
+//     keyup$.pipe(pluck('type')), 
+//     click$.pipe(pluck('type'))
+// ).subscribe( console.log )
+
+const input1: HTMLInputElement = document.createElement('input');
+const input2: HTMLInputElement = document.createElement('input');
+
+input1.placeholder = 'email@gmail.com';
+
+input2.placeholder = '*************';
+input2.type = 'password';
+
+document.body.append(input1, input2);
+
+// Helper
+const getInputStream = (elem: HTMLElement) => fromEvent<KeyboardEvent>(elem, 'keyup')
+    .pipe(
+        pluck<KeyboardEvent, string>('target', 'value')
+    );
+
+combineLatest(
+    getInputStream(input1),
+    getInputStream(input2)
+).subscribe(console.log);
